89947d6e0f92e48b4a2c7bb7ba75b5901460440d
saracassandra7 <saracassandra7@gmail.com>
2023-01-31 16:23:10 +0100

    [add] mail configuration

---
+++ resources/views/email/new-contact.blade.php
@@ -0,0 +1,9 @@
+<h1>Nuovo messaggio per Admin</h1>
+<p>
+    Hai ricevuto un nuovo messaggio: <br>
+
+    Nome : {{$lead->name}} <br>
+    Email : {{$lead->email}} <br>
+    Message: <br>
+    {{ $lead->message }}
+</p>
