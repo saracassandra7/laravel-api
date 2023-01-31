4b27a4f64f1f163048364a7b60171ee70bfe3c40
saracassandra7 <saracassandra7@gmail.com>
2023-01-31 16:53:11 +0100

    [add] form structure

---
+++ resources/js/components/ContactForm.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'ContactForm'
+
+}
+</script>
+
+<template>
+
+<div class="container">
+    <form>
+
+        <div class="form-group">
+            <label for="name">Nome</label>
+            <input v-model.trim="name" type="text" class="form-control" placeholder="Inserisci il tuo nome">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input v-model.trim="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserisci l'email">
+        </div>
+
+        <div class="form-group">
+            <label for="messaggio">Messaggio</label>
+            <textarea v-model.trim="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary my-3">Invia</button>
+    </form>
+</div>
+
+</template>
+
+
+<style>
+
+</style>
--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -1,12 +1,23 @@
 <script>
+import ContactForm from '../components/ContactForm.vue'
+
 export default {
-    name: 'Contacts'
+    name: 'Contacts',
+
+    components:{
+        ContactForm
+    }
 
 }
 </script>
 
 <template>
-<h1>Contacts</h1>
+<div class="container">
+<h1 class="text-center m-3">Contatti</h1>
+
+<ContactForm/>
+
+</div>
 
 </template>
 
